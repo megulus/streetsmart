1a7ca1045deca91540c6a0556e8a006631ea01e7
Meg Dahlgren <meg.dahlgren@gmail.com>
2024-07-15 18:04:25 -0400

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "taketwostreetsmart33b0d9d4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import './App.css'
+import { Amplify } from "aws-amplify";
+import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+import amplifyconfig from './amplifyconfiguration.json';
+
+Amplify.configure(amplifyconfig)
+
+function App() {
+  return (
+      <>
+          <div className="App">
+              <Authenticator>
+                  {({signOut}) => (
+                      <main>
+                          <header className='App-header'>
+                              <button
+                                  onClick={signOut}
+                                  style={{
+                                      margin: '20px',
+                                      fontSize: '0.8rem',
+                                      padding: '5px 10px',
+                                      marginTop: '20px'
+                                  }}
+                              >
+                                  Sign Out
+                              </button>
+                          </header>
+                      </main>
+                  )}
+              </Authenticator>
+          </div>
+      </>
+  )
+}
+
+export default withAuthenticator(App)
